21807fa9b815f0d00d0bfb1e5301a091034e5606
SeeSharpr <SeeSharpr@users.noreply.github.com>
2025-01-06 10:03:11 -0800

    Up to LengthOfLongestSubstring

---
+++ leetcode/Lists/Top150/SlidingWindow.cs
@@ -0,0 +1,77 @@
+﻿using System.Data.SqlTypes;
+
+namespace leetcode.Lists.Top150
+{
+    public class SlidingWindow
+    {
+        // Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target.
+        // If there is no such subarray, return 0 instead.
+        [Theory]
+        [InlineData(7, new[] { 2, 3, 1, 2, 4, 3 }, 2)]
+        [InlineData(4, new[] { 1, 4, 4 }, 1)]
+        [InlineData(11, new[] { 1, 1, 1, 1, 1, 1, 1, 1 }, 0)]
+        public void MinSubArrayLen(int target, int[] nums, int expected)
+        {
+            int n = nums.Length;
+            int left = 0;
+            int sum = 0;
+
+            int minLength = int.MaxValue;
+            for (int right = 0; right < n; right++)
+            {
+                sum += nums[right];
+
+                while (sum >= target && left <= right)
+                {
+                    minLength = Math.Min(minLength, right - left + 1);
+                    sum -= nums[left];
+                    left++;
+                }
+            }
+
+            if (minLength == int.MaxValue)
+            {
+                minLength = 0;
+            }
+
+            Assert.Equal(expected, minLength);
+        }
+
+        // Given a string s, find the length of the longest substring without repeating characters.
+        [Theory]
+        [InlineData("abcabcbb", 3)]
+        [InlineData("bbbbb", 1)]
+        [InlineData("pwwkew", 3)]
+        [InlineData("aab", 2)]
+        public void LengthOfLongestSubstring(string s, int expected)
+        {
+            HashSet<char> seen = new();
+
+            int maxLength = 0;
+            int currentLength = 0;
+            int left = 0;
+            for (int right = 0; right < s.Length; right++)
+            {
+                if (seen.Add(s[right]))
+                {
+                    currentLength++;
+                }
+                else
+                {
+                    while (left < right && s[left] != s[right])
+                    {
+                        seen.Remove(s[left]);
+                        left++;
+                        currentLength--;
+                    }
+
+                    left++;
+                }
+
+                maxLength = Math.Max(maxLength, currentLength);
+            }
+
+            Assert.Equal(expected, maxLength);
+        }
+    }
+}
--- leetcode/Lists/Top150/TwoPointers.cs
+++ leetcode/Lists/Top150/TwoPointers.cs
@@ -1,4 +1,7 @@
-﻿namespace leetcode.Lists.Top150
+﻿using System.Collections.Immutable;
+using Xunit.Sdk;
+
+namespace leetcode.Lists.Top150
 {
     public class TwoPointers
     {
@@ -61,6 +64,10 @@
             Assert.Equal(expected, result);
         }
 
+        // Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+        // Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+        // The tests are generated such that there is exactly one solution. You may not use the same element twice.
+        // Your solution must use only constant extra space.
         [Theory]
         [InlineData(new[] { 2, 7, 11, 15 }, 9, new[] { 1, 2 })]
         [InlineData(new[] { 2, 3, 4 }, 6, new[] { 1, 3 })]
@@ -94,6 +101,92 @@
             int[] result = new[] { i + 1, j + 1 };
 
             Assert.True(expected.SequenceEqual(result));
+        }
+
+        // You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+        // Find two lines that together with the x-axis form a container, such that the container contains the most water.
+        // Return the maximum amount of water a container can store.
+        [Theory]
+        [InlineData(new[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }, 49)]
+        [InlineData(new[] { 1, 1 }, 1)]
+        public void MaxArea(int[] height, int expected)
+        {
+            int maxArea = 0;
+            int left = 0;
+            int right = height.Length - 1;
+
+            while (left < right)
+            {
+                int width = right - left;
+                int minHeight = Math.Min(height[left], height[right]);
+                int currentArea = width * minHeight;
+                maxArea = Math.Max(maxArea, currentArea);
+
+                if (height[left] < height[right])
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            Assert.Equal(expected, maxArea);
+        }
+
+        // Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+        // Notice that the solution set must not contain duplicate triplets.
+        // Notice that the order of the output and the order of the triplets does not matter.
+        [Theory]
+        [InlineData(new[] { -1, 0, 1, 2, -1, -4 }, new[] { -1, -1, 2 }, new[] { -1, 0, 1 })]
+        [InlineData(new[] { 0, 1, 1 })]
+        [InlineData(new[] { 0, 0, 0 }, new[] { 0, 0, 0 })]
+        [InlineData(new[] { -2, 0, 1, 1, 2 }, new[] { -2, 0, 2 }, new[] { -2, 1, 1 })]
+        public void ThreeSum(int[] nums, params int[][] results)
+        {
+            List<IList<int>> result = new();
+
+            Array.Sort(nums);
+
+            int n = nums.Length - 2;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+                int left = i + 1;
+                int right = n + 1;
+
+                while (left < right)
+                {
+                    int sum = nums[i] + nums[left] + nums[right];
+
+                    if (sum < 0)
+                    {
+                        left++;
+                    }
+                    else if (sum > 0)
+                    {
+                        right--;
+                    }
+                    else // sum == 0
+                    {
+                        result.Add(new List<int>() { nums[i], nums[left], nums[right] });
+
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right - 1] == nums[right]) right--;
+
+                        left++;
+                        right--;
+                    }
+                }
+            }
+
+            HashSet<Tuple<int, int, int>> actual = result.Select(x => { int[] a = x.ToArray(); Array.Sort(a); return new Tuple<int, int, int>(a[0], a[1], a[2]); }).ToHashSet();
+            HashSet<Tuple<int, int, int>> expected = results.Select(x => { int[] a = x.ToArray(); Array.Sort(a); return new Tuple<int, int, int>(a[0], a[1], a[2]); }).ToHashSet();
+
+            Assert.Equal(expected, actual);
         }
     }
 }
