dc5051b2d94118e061e5315de0cdfcce1bf8244c
SeeSharpr <SeeSharpr@users.noreply.github.com>
2025-01-19 18:37:53 -0800

    Up to "IsAnagram"

--- leetcode/Lists/Top150/HashMap.cs
+++ leetcode/Lists/Top150/HashMap.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Metrics;
+using System.Reflection.Metadata;
 using System.Text;
 
 namespace leetcode.Lists.Top150
@@ -97,6 +99,138 @@ namespace leetcode.Lists.Top150
             bool tMinusSEmpty = isoMapT.Except(isoMapS).Count() == 0;
 
             bool result = sMinusTEmpty && tMinusSEmpty;
+
+            Assert.Equal(expected, result);
+        }
+
+        // Given a pattern and a string s, find if s follows the same pattern.
+        // Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.Specifically:
+        // Each letter in pattern maps to exactly one unique word in s.
+        // Each unique word in s maps to exactly one letter in pattern.
+        // No two letters map to the same word, and no two words map to the same letter.
+        [Theory]
+        [InlineData("abba", "dog cat cat dog", true)]
+        [InlineData("abba", "dog cat cat fish", false)]
+        [InlineData("aaaa", "dog cat cat dog", false)]
+        [InlineData("abba", "dog dog dog dog", false)]
+        public void WordPattern(string pattern, string s, bool expected)
+        {
+            Dictionary<char, string> map = new();
+            Dictionary<string, char> reverse = new();
+
+            string[] strings = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            bool result = pattern.Length == strings.Length;
+
+            if (result)
+            {
+                for (int i = 0; i < pattern.Length; i++)
+                {
+                    char p = pattern[i];
+                    string word = strings[i];
+
+                    if (!map.TryGetValue(p, out string? match))
+                    {
+                        if (reverse.ContainsKey(word))
+                        {
+                            result = false;
+                            break;
+                        }
+
+                        map[p] = word;
+                        reverse[word] = p;
+
+                    }
+                    else if (!string.Equals(word, match))
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+
+            //bool result = true;
+            //int pi = 0;
+            //int si = 0;
+            //while (si < s.Length && char.IsWhiteSpace(s[si])) si++;
+
+            //while (true)
+            //{
+            //    char p = pattern[pi];
+
+            //    int k = si;
+            //    while (k < s.Length && char.IsLetter(s[k])) k++;
+            //    string word = s.Substring(si, k - si);
+
+            //    while (k < s.Length && char.IsWhiteSpace(s[k])) k++;
+            //    si = k;
+
+            //    if (!map.TryGetValue(p, out string? match))
+            //    {
+            //        if (reverse.ContainsKey(word))
+            //        {
+            //            result = false;
+            //            break;
+            //        }
+
+            //        map[p] = word;
+            //        reverse[word] = p;
+
+            //    }
+            //    else if (!string.Equals(word, match))
+            //    {
+            //        result = false;
+            //        break;
+            //    }
+
+            //    pi++;
+
+            //    if (pi == pattern.Length && si == s.Length)
+            //    {
+            //        break;
+            //    }
+            //    else if (pi == pattern.Length || si == s.Length)
+            //    {
+            //        result = false;
+            //        break;
+            //    }
+            //}
+
+            Assert.Equal(expected, result);
+        }
+
+        // Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+        [Theory]
+        [InlineData("anagram", "nagaram", true)]
+        [InlineData("rat", "car", false)]
+        public void IsAnagram(string s, string t, bool expected)
+        {
+            int[] ss = new int[26];
+            int[] ts = new int[26];
+            bool result = s.Length == t.Length;
+
+            if (result)
+            {
+                foreach (char c in s)
+                {
+                    ss[c - 'a']++;
+                }
+
+                foreach (char c in t)
+                {
+                    int i = c - 'a';
+
+                    if (ss[i] == 0 || ss[i] <= ts[i])
+                    {
+                        result = false;
+                        break;
+                    }
+
+                    ts[i]++;
+                }
+
+                result = result && ss.SequenceEqual(ts);
+            }
 
             Assert.Equal(expected, result);
         }
