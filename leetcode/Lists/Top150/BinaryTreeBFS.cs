c6d691e0f8072fffd1ed5b9093096dba722ed6e7
SeeSharpr <SeeSharpr@users.noreply.github.com>
2025-02-21 13:59:06 -0800

    Up to "AverageOfLevels"

--- leetcode/Lists/Top150/BinaryTreeBFS.cs
+++ leetcode/Lists/Top150/BinaryTreeBFS.cs
@@ -1,4 +1,5 @@
 ﻿using leetcode.Types.BinaryTree;
+using Newtonsoft.Json.Linq;
 
 namespace leetcode.Lists.Top150
 {
@@ -17,11 +18,11 @@ namespace leetcode.Lists.Top150
             TreeNode? root = input.ParseLCTree(TreeNode.Create, TreeNode.Update);
             IList<int> expected = output.ParseArrayStringLC(int.Parse).ToList();
 
-            int previous = 0;
             List<int> actual = [];
             if (root != null)
             {
                 actual.Add(root.val);
+                int previous = 0;
                 Queue<KeyValuePair<int, TreeNode>> queue = new([new(0, root)]);
 
                 while (queue.TryDequeue(out KeyValuePair<int, TreeNode> pair))
@@ -35,6 +36,50 @@ namespace leetcode.Lists.Top150
                     if (pair.Value.right != null) queue.Enqueue(new KeyValuePair<int, TreeNode>(pair.Key + 1, pair.Value.right));
                     if (pair.Value.left != null) queue.Enqueue(new KeyValuePair<int, TreeNode>(pair.Key + 1, pair.Value.left));
                 }
+            }
+
+            Assert.Equal(expected, actual);
+        }
+
+        // 637. Average of Levels in Binary Tree
+        // Given the root of a binary tree, return the average value of the nodes on each level in the form of an array.Answers within 10-5 of the actual answer will be accepted.
+        [Trait("Difficulty", "Easy")]
+        [Theory]
+        [InlineData("[3,9,20,null,null,15,7]", "[3.00000,14.50000,11.00000]")]
+        [InlineData("[3,9,20,15,7]", "[3.00000,14.50000,11.00000]")]
+        public void AverageOfLevels(string input, string output)
+        {
+            TreeNode? root = input.ParseLCTree(TreeNode.Create, TreeNode.Update);
+            IList<double> expected = output.ParseArrayStringLC(double.Parse).ToList();
+
+            List<double> actual = [];
+
+            if (root != null)
+            {
+                Queue<KeyValuePair<int, TreeNode>> queue = new([new(0, root)]);
+                int current = 0;
+                int count = 0;
+                double sum = 0;
+                while (queue.TryDequeue(out KeyValuePair<int, TreeNode> pair))
+                {
+                    if (pair.Key == current)
+                    {
+                        count++;
+                        sum += pair.Value.val;
+                    }
+                    else
+                    {
+                        actual.Add(sum / count);
+                        current = pair.Key;
+                        sum = pair.Value.val;
+                        count = 1;
+                    }
+
+                    if (pair.Value.left != null) queue.Enqueue(new(pair.Key + 1, pair.Value.left));
+                    if (pair.Value.right != null) queue.Enqueue(new(pair.Key + 1, pair.Value.right));
+                }
+
+                if (count > 0) actual.Add(sum / count);
             }
 
             Assert.Equal(expected, actual);
