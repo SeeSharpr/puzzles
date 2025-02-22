10c5b0acc0b1115c86e60981fadb8d9ff7cb23c8
SeeSharpr <SeeSharpr@users.noreply.github.com>
2025-02-21 17:40:17 -0800

    Up to "IsValidBST"

--- leetcode/Lists/Top150/BinarySearchTree.cs
+++ leetcode/Lists/Top150/BinarySearchTree.cs
@@ -1,6 +1,7 @@
 ﻿using leetcode.Types.BinaryTree;
 using leetcode.Types.LinkedList;
 using System;
+using System.ComponentModel;
 
 namespace leetcode.Lists.Top150
 {
@@ -72,7 +73,7 @@ namespace leetcode.Lists.Top150
             //InternalKthSmallest(root, k, ref count, ref actual);
 
             TreeNode? node = root;
-            for (Stack<TreeNode> stack = node == null ? new() : new([node]); ;)
+            for (Stack<TreeNode> stack = new(); ;)
             {
                 for (; node != null; node = node.left) stack.Push(node);
                 node = stack.Pop();
@@ -81,7 +82,40 @@ namespace leetcode.Lists.Top150
             }
 
             int actual = node.val;
-            
+
+            Assert.Equal(expected, actual);
+        }
+
+        // 98. Validate Binary Search Tree
+        // Given the root of a binary tree, determine if it is a valid binary search tree(BST).
+        // A valid BST is defined as follows:
+        // The left subtree of a node contains only nodes with keys less than the node's key.
+        // The right subtree of a node contains only nodes with keys greater than the node's key.
+        // Both the left and right subtrees must also be binary search trees.
+        [Trait("Difficulty", "Medium")]
+        [Theory]
+        [InlineData("[2,1,3]", true)]
+        [InlineData("[5,1,4,null,null,3,6]", false)]
+        [InlineData("[5,4,6,null,null,3,7]", false)]
+        [InlineData("[2,2,2]", false)]
+        public void IsValidBST(string input, bool expected)
+        {
+            TreeNode? root = input.ParseLCTree(TreeNode.Create, TreeNode.Update);
+
+            static bool InternalIsValidBST(TreeNode? node, ref TreeNode? limit)
+            {
+                if (node == null) return true;
+
+                if (!InternalIsValidBST(node.left, ref limit)) return false;
+                if (limit != null && node.val <= limit.val) return false;
+                limit = node;
+
+                return InternalIsValidBST(node.right, ref limit);
+            }
+
+            TreeNode? limit = null;
+            bool actual = InternalIsValidBST(root, ref limit);
+
             Assert.Equal(expected, actual);
         }
     }
