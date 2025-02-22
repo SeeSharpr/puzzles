0194de2b22215674f025cdecbfa85c0308360fe1
SeeSharpr <SeeSharpr@users.noreply.github.com>
2025-02-21 17:55:52 -0800

    Moved "NumIslands" to the correct file.

--- leetcode/Lists/Top150/TreesGraphs.cs
+++ leetcode/Lists/Top150/TreesGraphs.cs
@@ -4,44 +4,6 @@ namespace leetcode.Lists.Top150
 {
     public class TreesGraphs
     {
-        // 200. Number of Islands
-        // Given an m x n 2D binary grid grid which represents a map of '1's(land) and '0's(water)| return the number of islands.
-        // An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
-        [Trait("Company", "Amazon")]
-        [Theory]
-        [InlineData("1,1,1,1,0|1,1,0,1,0|1,1,0,0,0|0,0,0,0,0", 1)]
-        [InlineData("1,1,0,0,0|1,1,0,0,0|0,0,1,0,0|0,0,0,1,1", 3)]
-        [InlineData("1,0,1,1,0,1,1", 3)]
-        public void NumIslands(string input, int expected)
-        {
-            char[][] grid = input.ParseNestedArrayStringLC(char.Parse).Select(e => e.ToArray()).ToArray();
-
-            static void NukeIsland(char[][] grid, int x, int y)
-            {
-                grid[y][x] = '0';
-
-                if (x - 1 >= 0 && grid[y][x - 1] == '1') NukeIsland(grid, x - 1, y);
-                if (x + 1 < grid[y].Length && grid[y][x + 1] == '1') NukeIsland(grid, x + 1, y);
-                if (y - 1 >= 0 && grid[y - 1][x] == '1') NukeIsland(grid, x, y - 1);
-                if (y + 1 < grid.Length && grid[y + 1][x] == '1') NukeIsland(grid, x, y + 1);
-            }
-
-            int actual = 0;
-            for (int y = 0; y < grid.Length; y++)
-            {
-                for (int x = 0; x < grid[y].Length; x++)
-                {
-                    if (grid[y][x] == '1')
-                    {
-                        actual++;
-                        NukeIsland(grid, x, y);
-                    }
-                }
-            }
-
-            Assert.Equal(expected, actual);
-        }
-
         // 297. Serialize and Deserialize Binary Tree
         // Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
         // Design an algorithm to serialize and deserialize a binary tree.There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure.
