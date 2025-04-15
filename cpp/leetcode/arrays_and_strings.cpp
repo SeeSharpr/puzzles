c9dc20b1c0b2630d8b43b479d7d81dc148a0a851
SeeSharpr <seesharpr@github.com>
2025-04-14 21:27:29 -0700

    Up to "minimumOperations"

--- cpp/leetcode/arrays_and_strings.cpp
+++ cpp/leetcode/arrays_and_strings.cpp
@@ -1,12 +1,10 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 #include <tuple>
+#include <unordered_set>
 #include <vector>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-using std::get;
-using std::tuple;
-using std::vector;
 
 #define TEST_LC_DIFFICULTY(traitValue) TEST_CLASS_ATTRIBUTE(L"Difficulty", traitValue)
 
@@ -20,10 +18,10 @@ namespace leetcode
 		END_TEST_CLASS_ATTRIBUTE()
 
 		// 27. Remove Element
-		int removeElement(vector<int>& nums, int val) {
-			int left = 0;
+		int removeElement(std::vector<int>& nums, int val) {
+		int left = 0;
 			int right = 0;
-			for (int limit = nums.size(); right < limit; right++)
+			for (size_t limit = nums.size(); right < limit; right++)
 			{
 				if (nums[right] != val)
 				{
@@ -36,7 +34,7 @@ namespace leetcode
 
 		TEST_METHOD(test_removeElement)
 		{
-			vector<tuple<vector<int>, int, vector<int>>> params =
+			std::vector<std::tuple<std::vector<int>, int, std::vector<int>>> params =
 			{
 				{{3, 2, 2, 3}, 3, {2, 2}},
 				{{0, 1, 2, 2, 3, 0, 4, 2}, 2, {0, 1, 3, 0, 4}},
@@ -44,9 +42,9 @@ namespace leetcode
 
 			for (auto& tuple : params)
 			{
-				auto& nums = get<0>(tuple);
-				auto& val = get<1>(tuple);
-				auto& expected = get<2>(tuple);
+				auto& nums = std::get<0>(tuple);
+				auto& val = std::get<1>(tuple);
+				auto& expected = std::get<2>(tuple);
 
 				Assert::AreEqual((int)expected.size(), removeElement(nums, val));
 				for (int i = 0; i < expected.size(); i++)
@@ -57,7 +55,7 @@ namespace leetcode
 		}
 
 		// 88. Merge Sorted Array
-		static void merge(vector<int>& nums1, int m, vector<int>& nums2, int n)
+		static void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n)
 		{
 			if (n == 0) return;
 
@@ -76,7 +74,7 @@ namespace leetcode
 
 		TEST_METHOD(test_merge)
 		{
-			vector<tuple<vector<int>, int, vector<int>, int, vector<int>>> params =
+			std::vector<std::tuple<std::vector<int>, int, std::vector<int>, int, std::vector<int>>> params =
 			{
 				{{1,2,3,0,0,0}, 3, {2,5,6}, 3, {1,2,2,3,5,6}},
 				{{1},1,{},0,{1}},
@@ -85,11 +83,11 @@ namespace leetcode
 
 			for (auto& tuple : params)
 			{
-				auto nums1 = get<0>(tuple);
-				auto m = get<1>(tuple);
-				auto nums2 = get<2>(tuple);
-				auto n = get<3>(tuple);
-				auto expected = get<4>(tuple);
+				auto nums1 = std::get<0>(tuple);
+				auto m = std::get<1>(tuple);
+				auto nums2 = std::get<2>(tuple);
+				auto n = std::get<3>(tuple);
+				auto expected = std::get<4>(tuple);
 
 				merge(nums1, m, nums2, n);
 
@@ -98,6 +96,38 @@ namespace leetcode
 					Assert::AreEqual(expected[i], nums1[i]);
 				}
 			}
+		}
+
+		// 2357. Make Array Zero by Subtracting Equal Amounts
+		static int minimumOperations(std::vector<int>& nums) {
+			std::unordered_set<int> set{ 0 };
+
+			int count = 0;
+			for (const int value : nums)
+			{
+				auto ir = set.insert(value);
+				if (ir.second == true) count++;
+			}
+
+			return count;
+		}
+
+		TEST_METHOD(test_minimumOperations)
+		{
+			std::vector<std::tuple<std::vector<int>, int>> params =
+			{
+				{{1,5,0,3,5}, 3},
+				{{0}, 0},
+			};
+
+			for (auto& tuple : params)
+			{
+				auto nums = std::get<0>(tuple);
+				auto expected = std::get<1>(tuple);
+
+				Assert::AreEqual(expected, minimumOperations(nums));
+			}
+
 		}
 	};
 }
