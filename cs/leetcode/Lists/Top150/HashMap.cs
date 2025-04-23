fbc84ae853fa8a29499224e0c53e4a86ddcd0305
SeeSharpr <SeeSharpr@users.noreply.github.com>
2025-04-22 23:13:34 -0700

    MostVisitedPattern

--- cs/leetcode/Lists/Top150/HashMap.cs
+++ cs/leetcode/Lists/Top150/HashMap.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Runtime.InteropServices;
 using System.Text;
+using static System.Formats.Asn1.AsnWriter;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace leetcode.Lists.Top150
 {
@@ -104,6 +109,87 @@ namespace leetcode.Lists.Top150
                     Assert.Empty(sortedExpected);
                     Assert.Empty(sortedExpected);
                 }
+            }
+
+            /// <summary>
+            /// 1152. Analyze User Website Visit Pattern
+            /// You are given two string arrays username and website and an integer array timestamp.All the given arrays are of the same length and the tuple[username[i], website[i], timestamp[i]] indicates that the user username[i] visited the website website[i] at time timestamp[i].
+            /// A pattern is a list of three websites (not necessarily distinct).
+            /// For example, ["home", "away", "love"], ["leetcode", "love", "leetcode"], and["luffy", "luffy", "luffy"] are all patterns.
+            /// The score of a pattern is the number of users that visited all the websites in the pattern in the same order they appeared in the pattern.
+            /// For example, if the pattern is ["home", "away", "love"], the score is the number of users x such that x visited "home" then visited "away" and visited "love" after that.
+            /// Similarly, if the pattern is ["leetcode", "love", "leetcode"], the score is the number of users x such that x visited "leetcode" then visited "love" and visited "leetcode" one more time after that.
+            /// Also, if the pattern is ["luffy", "luffy", "luffy"], the score is the number of users x such that x visited "luffy" three different times at different timestamps.
+            /// Return the pattern with the largest score.If there is more than one pattern with the same largest score, return the lexicographically smallest such pattern.
+            /// Note that the websites in a pattern do not need to be visited contiguously, they only need to be visited in the order they appeared in the pattern.
+            /// </summary>
+            /// <see cref="https://leetcode.com/problems/analyze-user-website-visit-pattern"/>
+            [Theory]
+            [InlineData("[joe,joe,joe,james,james,james,james,mary,mary,mary]", "[1,2,3,4,5,6,7,8,9,10]", "[home,about,career,home,cart,maps,home,home,about,career]", "[home,about,career]")]
+            [InlineData("[ua,ua,ua,ub,ub,ub]", "[1,2,3,4,5,6]", "[a,b,a,a,b,c]", "[a,b,a]")]
+            [InlineData("[zkiikgv,zkiikgv,zkiikgv,zkiikgv]", "[436363475,710406388,386655081,797150921]", "[wnaaxbfhxp,mryxsjc,oz,wlarkzzqht]", "[oz,mryxsjc,wlarkzzqht]")]
+            public void MostVisitedPattern(string inputUsername, string inputTimestamp, string inputWebsite, string output)
+            {
+                string[] username = inputUsername.Parse1DArray().ToArray();
+                int[] timestamp = inputTimestamp.Parse1DArray(int.Parse).ToArray();
+                string[] website = inputWebsite.Parse1DArray().ToArray();
+                IList<string> expected = output.Parse1DArray().ToList();
+                // -
+
+                static IList<string> WithDictionary(string[] username, int[] timestamp, string[] website)
+                {
+                    // First, build the tuples
+                    Dictionary<string, SortedList<int, string>> tuples = [];
+                    for (int i = 0; i < username.Length; i++)
+                    {
+                        if (!tuples.TryGetValue(username[i], out var list))
+                        {
+                            tuples[username[i]] = list = [];
+                        }
+
+                        list.Add(timestamp[i], website[i]);
+                    }
+
+                    // Next, build the patterns for each user
+                    string? maxPattern = null;
+                    int maxCount = int.MinValue;
+                    Dictionary<string, HashSet<string>> patterns = new();
+                    foreach (var tuple in tuples)
+                    {
+                        string user = tuple.Key;
+                        IList<string> sites = tuple.Value.Values;
+
+                        for (int i = 0; i < sites.Count - 2; i++)
+                        {
+                            for (int j = i + 1; j < sites.Count - 1; j++)
+                            {
+                                for (int k = j + 1; k < sites.Count; k++)
+                                {
+                                    string pattern = $"{sites[i]};{sites[j]};{sites[k]}";
+
+                                    if (!patterns.TryGetValue(pattern, out var users))
+                                    {
+                                        patterns[pattern] = users = [];
+                                    }
+
+                                    users.Add(user);
+
+                                    if (users.Count > maxCount || (users.Count == maxCount && (maxPattern?.CompareTo(pattern) >= 0)))
+                                    {
+                                        maxPattern = pattern;
+                                        maxCount = users.Count;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    return maxPattern?.Split(';') ?? [];
+                }
+
+                IList<string> actual = WithDictionary(username, timestamp, website);
+
+                Assert.Equal(expected, actual);
             }
         }
 
