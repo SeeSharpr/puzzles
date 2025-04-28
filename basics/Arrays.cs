c4439661eafa565b1ee6fbda6a511fddb55f8d49
SeeSharpr <SeeSharpr@users.noreply.github.com>
2025-04-28 16:18:47 -0700

    MergeSort

---
+++ basics/Arrays.cs
@@ -0,0 +1,23 @@
+﻿namespace basics
+{
+    public class Arrays
+    {
+        [Theory]
+        [InlineData()]
+        [InlineData(1)]
+        [InlineData(2, 3, 1)]
+        [InlineData(3, 7, 5, 2, 1, 4, 9, 8, 6)]
+        public void MergeSort(params int[] nums)
+        {
+            static int[] DoSort(int[] src, int[] dst, int left, int right)
+            {
+                if (left == right) return src;
+            }
+
+            int[] expected = nums.Order().ToArray();
+            int[] actual = DoSort(nums, new int[nums.Length], 0, nums.Length - 11);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
